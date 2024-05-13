008018783619f3e692fce69af02d60d79060e2c8
David07994415 <david.game.power@gmail.com>
2024-05-13 15:13:55 +0800

    Update:Adjust LiveController format

--- FarmerPro/Controllers/LiveController.cs
+++ FarmerPro/Controllers/LiveController.cs
@@ -19,7 +19,6 @@ namespace FarmerPro.Controllers
         private FarmerProDB db = new FarmerProDB();
 
         #region FCL-01 取得目前直播內容(包含取得近期直播內容)
-
         /// <summary>
         /// FCL-01 取得目前直播內容(包含取得近期直播內容)
         /// </summary>
@@ -32,8 +31,8 @@ namespace FarmerPro.Controllers
             try
             {
                 var CurrentLiveEvent = db.LiveSettings.AsEnumerable()
-                    .Where(x => x.Id == 47) // Fix Render Data
-                                            //.Where(x => x.LiveDate.Date == DateTime.Now.Date && (x.StartTime.Hours <= DateTime.Now.Hour && x.StartTime.Minutes <= DateTime.Now.Minute &&  x.EndTime.Hours > DateTime.Now.Hour && x.EndTime.Minutes <= DateTime.Now.Minute))
+                    .Where(x => x.Id == 47)  // Fix to render the data
+                                             //.Where(x => x.LiveDate.Date == DateTime.Now.Date && (x.StartTime.Hours <= DateTime.Now.Hour && x.StartTime.Minutes <= DateTime.Now.Minute &&  x.EndTime.Hours > DateTime.Now.Hour && x.EndTime.Minutes <= DateTime.Now.Minute))
                     .Select(liveSetting => new
                     {
                         LiveSetting = liveSetting,
@@ -42,20 +41,10 @@ namespace FarmerPro.Controllers
                                            .OrderByDescending(album => album.CreateTime)
                                            .FirstOrDefault()
                     })
-                    //.Join(db.LiveAlbum,
-                    //      liveSetting => liveSetting.Id,
-                    //      liveAlbum => liveAlbum.LiveId,
-                    //      (liveSetting, liveAlbum) => new
-                    //      {
-                    //          LiveSetting = liveSetting,
-                    //          LiveAlbum = liveAlbum
-                    //      })
-                    //.GroupBy(x => x.LiveSetting)  // 依據LiveSetting進行分組
-                    //.Select(g => g.First())  // 每組取1個，確保LiveSettings對上一個
                     .FirstOrDefault();
 
                 var UpcomingLiveEvent = db.LiveSettings.AsEnumerable()
-                    .Where(x => x.LiveDate.Date >= new DateTime(2024, 03, 09))  // Fix Render Data
+                    .Where(x => x.LiveDate.Date >= new DateTime(2024, 03, 09))  // Fix to render the data
                                                                                 //.Where(x => (x.LiveDate.Date == DateTime.Now.Date && x.StartTime.Hours > DateTime.Now.Hour) || x.LiveDate.Date > DateTime.Now.Date)
                     .OrderBy(x => x.LiveDate)
                     .Select(liveSetting => new
@@ -66,19 +55,9 @@ namespace FarmerPro.Controllers
                         .OrderByDescending(album => album.CreateTime)
                         .FirstOrDefault()
                     })
-                    //.Join(db.LiveAlbum,
-                    //      liveSetting => liveSetting.Id,
-                    //      liveAlbum => liveAlbum.LiveId,
-                    //      (liveSetting, liveAlbum) => new
-                    //      {
-                    //          LiveSetting = liveSetting,
-                    //          LiveAlbum = liveAlbum
-                    //      })
-                    //.GroupBy(x => x.LiveSetting)  // 依據LiveSetting進行分組
-                    //.Select(g => g.First())              // 每組取1個，確保LiveSettings對上一個
                     .Take(6);
 
-                if (CurrentLiveEvent == null)  // 沒有現正直播
+                if (CurrentLiveEvent == null)  // 若現正沒有直播場次
                 {
                     var result = new
                     {
@@ -95,7 +74,6 @@ namespace FarmerPro.Controllers
                                 livePrice = eventItem.LiveSetting.LiveProduct.FirstOrDefault()?.Spec.LivePrice,
                                 liveFarmer = eventItem.LiveSetting.LiveProduct.FirstOrDefault()?.Spec.Product.Users.NickName,
                                 liveFarmerPic = eventItem.LiveSetting.LiveProduct.FirstOrDefault()?.Spec.Product.Users.Photo,
-                                //livePic = eventItem.LivePic?.FirstOrDefault(),
                                 livePic = eventItem.LiveAlbum?.Photo,
                                 liveTime = eventItem.LiveSetting.LiveDate.Date.ToString("yyyy.MM.dd") + " " + SwitchDayofWeek(eventItem.LiveSetting.LiveDate.DayOfWeek) + " " + eventItem.LiveSetting.StartTime.ToString().Substring(0, 5),
                             }).ToList()
@@ -103,7 +81,7 @@ namespace FarmerPro.Controllers
                     };
                     return Content(HttpStatusCode.OK, result);
                 }
-                else                                        // 有現正直播
+                else  // 若現正有直播
                 {
                     var result = new
                     {
@@ -126,7 +104,6 @@ namespace FarmerPro.Controllers
                                 livePrice = eventItem.LiveSetting.LiveProduct.FirstOrDefault()?.Spec.LivePrice,
                                 liveFarmer = eventItem.LiveSetting.LiveProduct.FirstOrDefault()?.Spec.Product.Users.NickName,
                                 liveFarmerPic = eventItem.LiveSetting.LiveProduct.FirstOrDefault()?.Spec.Product.Users.Photo,
-                                //livePic = eventItem.LivePic?.FirstOrDefault(),
                                 livePic = eventItem.LiveAlbum?.Photo,
                                 liveTime = eventItem.LiveSetting.LiveDate.Date.ToString("yyyy.MM.dd") + " " + SwitchDayofWeek(eventItem.LiveSetting.LiveDate.DayOfWeek) + " " + eventItem.LiveSetting.StartTime.ToString().Substring(0, 5),
                             }).ToList()
@@ -150,7 +127,6 @@ namespace FarmerPro.Controllers
         #endregion FCL-01 取得目前直播內容(包含取得近期直播內容)
 
         #region FCL-02 取得特定直播場次內容(包含庫存資料)
-
         /// <summary>
         /// FCL-02 取得特定直播場次內容(包含庫存資料)
         /// </summary>
@@ -160,89 +136,88 @@ namespace FarmerPro.Controllers
         [Route("api/live/{liveId}")]
         public IHttpActionResult RenderLiveEvent(int liveId)
         {
-            //try
-            //{
-            var LiveEvent = db.LiveSettings.Where(x => x.Id == liveId)?.AsEnumerable().FirstOrDefault();
-            if (LiveEvent == null)
+            try
             {
-                var result = new
+                var LiveEvent = db.LiveSettings.Where(x => x.Id == liveId)?.AsEnumerable().FirstOrDefault();
+                if (LiveEvent == null)
                 {
-                    statusCode = 401,
-                    status = "error",
-                    message = "沒有此直播Id",
-                };
-                return Content(HttpStatusCode.OK, result);
-            }
-            else
-            {
-                if (LiveEvent.Id == 47) // Fix Render Data
-                                        //if (LiveEvent.LiveDate.Date == DateTime.Now.Date && (LiveEvent.StartTime.Hours <= DateTime.Now.Hour && LiveEvent.StartTime.Minutes <= DateTime.Now.Minute && LiveEvent.EndTime.Hours > DateTime.Now.Hour && LiveEvent.EndTime.Minutes <= DateTime.Now.Minute))
-                {
-                    var topproduct = db.LiveProducts.Where(x => x.LiveSettingId == liveId && x.IsTop == true)?.FirstOrDefault();
-                    int topproductspecId = 0;
-                    if (topproduct != null) { topproductspecId = topproduct.Spec.ProductId; };
                     var result = new
                     {
-                        statusCode = 200,
-                        status = "success",
-                        message = "取得成功",
-                        data = new
-                        {
-                            liveId = LiveEvent.Id,
-                            yturl = LiveEvent.ShareURL,
-                            liveName = LiveEvent.LiveName,
-                            liveFarmerId = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Id,
-                            liveFarmer = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.NickName == null ?
-                                                   db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Account.ToString().Substring(0, 2) + "小農"
-                                                   : db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.NickName,
-                            liveFarmerPic = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Photo,
-                            liveDate = LiveEvent.LiveDate.Date.ToString("yyyy.MM.dd") + " " + SwitchDayofWeek(LiveEvent.LiveDate.DayOfWeek) + " " + LiveEvent.StartTime.ToString().Substring(0, 5),
-                            endTime = LiveEvent.EndTime.ToString().Substring(0, 5),
-                            liveDescription = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Description,
-                            topProductId = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.Spec.ProductId,
-                            topProductName = db.Products.Where(x => x.Id == topproductspecId)?.FirstOrDefault()?.ProductTitle,
-                            topSpecId = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.SpecId,
-                            topProductStock = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.Spec.Stock,
-                            topProductLivePrice = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.Spec.LivePrice,
-                            topProductPhoto = db.Albums.Where(x => x.ProductId == topproductspecId)?.FirstOrDefault()?.Photo?.FirstOrDefault()?.URL,
-                            liveProductList = LiveEvent.LiveProduct.Select(y => new
-                            {
-                                productId = y.Spec?.ProductId,
-                                productName = db.Products.Where(k => k.Id == y.Spec.ProductId)?.FirstOrDefault().ProductTitle,
-                                specId = y.SpecId,
-                                productStock = y.Spec?.Stock,
-                                productLivePrice = y.Spec?.LivePrice,
-                                productOriginPrice = y.Spec?.Price,
-                                productPhoto = db.Albums.Where(z => z.ProductId == y.Spec.ProductId)?.FirstOrDefault()?.Photo?.FirstOrDefault()?.URL,
-                            }).ToList()
-                        }
+                        statusCode = 401,
+                        status = "error",
+                        message = "沒有此直播Id",
                     };
                     return Content(HttpStatusCode.OK, result);
                 }
                 else
                 {
-                    var result = new
+                    if (LiveEvent.Id == 47) // Fix for rendering data
+                    //if (LiveEvent.LiveDate.Date == DateTime.Now.Date && (LiveEvent.StartTime.Hours <= DateTime.Now.Hour && LiveEvent.StartTime.Minutes <= DateTime.Now.Minute && LiveEvent.EndTime.Hours > DateTime.Now.Hour && LiveEvent.EndTime.Minutes <= DateTime.Now.Minute))
                     {
-                        statusCode = 402,
-                        status = "error",
-                        message = "此直播Id目前尚未到達直播時間",
-                    };
-                    return Content(HttpStatusCode.OK, result);
+                        var topproduct = db.LiveProducts.Where(x => x.LiveSettingId == liveId && x.IsTop == true)?.FirstOrDefault();
+                        int topproductspecId = 0;
+                        if (topproduct != null) { topproductspecId = topproduct.Spec.ProductId; };
+                        var result = new
+                        {
+                            statusCode = 200,
+                            status = "success",
+                            message = "取得成功",
+                            data = new
+                            {
+                                liveId = LiveEvent.Id,
+                                yturl = LiveEvent.ShareURL,
+                                liveName = LiveEvent.LiveName,
+                                liveFarmerId = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Id,
+                                liveFarmer = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.NickName == null ?
+                                                       db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Account.ToString().Substring(0, 2) + "小農"
+                                                       : db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.NickName,
+                                liveFarmerPic = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Photo,
+                                liveDate = LiveEvent.LiveDate.Date.ToString("yyyy.MM.dd") + " " + SwitchDayofWeek(LiveEvent.LiveDate.DayOfWeek) + " " + LiveEvent.StartTime.ToString().Substring(0, 5),
+                                endTime = LiveEvent.EndTime.ToString().Substring(0, 5),
+                                liveDescription = db.Users.Where(x => x.Id == LiveEvent.UserId)?.FirstOrDefault()?.Description,
+                                topProductId = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.Spec.ProductId,
+                                topProductName = db.Products.Where(x => x.Id == topproductspecId)?.FirstOrDefault()?.ProductTitle,
+                                topSpecId = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.SpecId,
+                                topProductStock = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.Spec.Stock,
+                                topProductLivePrice = LiveEvent.LiveProduct.Where(x => x.IsTop == true)?.FirstOrDefault()?.Spec.LivePrice,
+                                topProductPhoto = db.Albums.Where(x => x.ProductId == topproductspecId)?.FirstOrDefault()?.Photo?.FirstOrDefault()?.URL,
+                                liveProductList = LiveEvent.LiveProduct.Select(y => new
+                                {
+                                    productId = y.Spec?.ProductId,
+                                    productName = db.Products.Where(k => k.Id == y.Spec.ProductId)?.FirstOrDefault().ProductTitle,
+                                    specId = y.SpecId,
+                                    productStock = y.Spec?.Stock,
+                                    productLivePrice = y.Spec?.LivePrice,
+                                    productOriginPrice = y.Spec?.Price,
+                                    productPhoto = db.Albums.Where(z => z.ProductId == y.Spec.ProductId)?.FirstOrDefault()?.Photo?.FirstOrDefault()?.URL,
+                                }).ToList()
+                            }
+                        };
+                        return Content(HttpStatusCode.OK, result);
+                    }
+                    else
+                    {
+                        var result = new
+                        {
+                            statusCode = 402,
+                            status = "error",
+                            message = "此直播Id目前尚未到達直播時間",
+                        };
+                        return Content(HttpStatusCode.OK, result);
+                    }
                 }
             }
-            //}
-            //catch
-            //{
-            //    var result = new
-            //    {
-            //        statusCode = 500,
-            //        status = "error",
-            //        message = "其他錯誤",
-            //    };
-            //    return Content(HttpStatusCode.OK, result);
-            //}
+            catch
+            {
+                var result = new
+                {
+                    statusCode = 500,
+                    status = "error",
+                    message = "其他錯誤",
+                };
+                return Content(HttpStatusCode.OK, result);
+            }
         }
-
         #endregion FCL-02 取得特定直播場次內容(包含庫存資料)
 
         public string SwitchDayofWeek(DayOfWeek input)
@@ -279,16 +254,6 @@ namespace FarmerPro.Controllers
                     break;
             }
             return chineseDayOfWeek.ToString();
-        }//星期的中文轉換方法
-
-        //測試用途api
-        [HttpGet]
-        [Route("api/liveAlbum/")]
-        public IHttpActionResult RenderLiveSession1()
-        {
-            var CurrentLiveEvent = db.LiveAlbum.ToList();
-
-            return Content(HttpStatusCode.OK, CurrentLiveEvent);
         }
     }
 }
