b7675efa8e5a8c4a42a901f38d7b1f01dd8a85ae
David07994415 <david.game.power@gmail.com>
2024-05-13 12:56:22 +0800

    Update:Adject ChatRoom and Record Model format

--- FarmerPro/Models/Record.cs
+++ FarmerPro/Models/Record.cs
@@ -26,18 +26,16 @@ namespace FarmerPro.Models
         [Display(Name = "已讀狀態")]
         public bool IsRead { get; set; } = false;
 
-
         [Display(Name = "建立時間")]
         public DateTime CreatTime { get; set; } = DateTime.Now;
 
-
         [Display(Name = "1-1聊天室Id")]
         public int ChatRoomId { get; set; }
-        [JsonIgnore]//不會產生無限迴圈
+
+        [JsonIgnore]
         [ForeignKey("ChatRoomId")]
         [Display(Name = "1-1聊天室")]
-        public virtual ChatRoom ChatRoom { get; set; }//virtual=虛擬資料，會跟資料庫的對應資料相對應
-
+        public virtual ChatRoom ChatRoom { get; set; }
 
     }
 }
